0609a109db2d39c1de04439c5905b748361f6836
philip <>
2001-03-27 23:57:37 +0000

    *** empty log message ***

--- scripts/one-structure.sh
+++ scripts/one-structure.sh
@@ -3,18 +3,19 @@
 ## This script produces all the stuff necessary for analyzing one particular protein
 ## structure. The pdb filename must be given as a parameter.
 
-
-
-quilt_home=$HOME/quilt                  # change to needs
-# quilt_path=$quilt_home/scripts:$quilt_home # if binary not installed in $PATH
-quilt_path=$quilt_home/scripts          # just for scripts
-
-PATH=$quilt_path:$PATH
+### Note: quilt and the scripts are assumed to be on the $PATH. If this is
+### not the case, uncomment this:
+# quilt_home=$HOME/quilt                  # change to needs
+# # quilt_path=$quilt_home/scripts:$quilt_home # if binary not installed in $PATH
+# quilt_path=$quilt_home/scripts          # just for scripts
+# PATH=$quilt_path:$PATH
 
 pdbcode=$1                              # assumes something.pdb
 pdbfile=$pdbcode.pdb
-## It is searched for in PDBPATH, so set that to at least include "."
-export PDBPATH=.
+## It is searched for in PDBPATH.Provide sensible default
+if [ ${PDBPATH-notdefined} = notdefined ]; then
+    export PDBPATH=.:..:$HOME
+fi
 
 areafile=$pdbcode.area
 patfile=$pdbcode.pat
@@ -22,6 +23,7 @@ logfile=$pdbcode.log
 histofile=$pdbcode.histo
 ranhistofile=$pdbcode.ranhisto
 tmpdir=./tmp                            # where all the randomized pat files go
+mkdir $tmpdir 2>/dev/null               # create if needed
 textfile=$pdbcode.txt
 
 polar_expansion=1.4
@@ -44,11 +46,11 @@ done > $pdbcode.rasmol
 # produce histograms and randomized histograms to assess significance:
 awk '/^# [0-9]/{print $10}' $patfile | histo -l 0 -s 10 > $histofile
 
-for i in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 
+for i in 0 1 2 3 4 5 6 7 8 9 
 do 
-  quilt -ran -n 252 -ep 1.4 -R -p $areafile > $tmpdir/$pdbcode.ran$i 2> /dev/null
-done
+  quilt -ran -n 252 -ep 1.4 -R -p $areafile > $tmpdir/$pdbcode.ran$i 
+done 2>> $logfile
 
-cat $tmpdir/$pdbcode.ran[01][0-9] | awk '/^# [0-9]/{print $10}' | histo -l 0 -s 10 \
-  | awk '{print $1, $2/20.0}' > $ranhistofile
+cat $tmpdir/$pdbcode.ran[0-9] | awk '/^# [0-9]/{print $10}' | histo -l 0 -s 10 \
+  | awk '{print $1, $2/10.0}' > $ranhistofile
 
