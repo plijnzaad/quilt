b1de906593262abea35408cdd356d9daf3ae5a6c
philip <>
2009-11-30 15:05:52 +0000

    renamed job to examples-job.sh

---
+++ examples/examples-job.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -x
+# $Id$
+
+# script to produce the output in the example directory
+
+tmpdir=t
+mkdir $tmpdir 2>/dev/null
+cd $tmpdir
+echo "will do work in `pwd` to avoid overwriting the distribution's examples"
+
+quilt_home=`pwd`/../..
+quilt_scripts=$quilt_home/scripts
+PATH=$quilt_scripts:$PATH
+export PDBPATH=..:.
+
+one-structure.sh 8lyz
+
+
+
+
+
+
