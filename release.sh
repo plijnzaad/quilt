e0f7ce5f23eecd74e3986be592d4e40ff720a857
philip <>
2011-11-15 13:25:27 +0000

    change of cvsroot

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@
 # The version number is contained within the file VERSION.
 
 # where to find stuff:
-privcvs=":ext:gen-fs.genomicscenter.nl:/home/gen/philip/cvsroot"
+privcvs=":ext:login.genomicscenter.nl:/home/gen/philip/cvsroot"
 # where is the histo script:
 histo=/home/philip/perl/histo
 
